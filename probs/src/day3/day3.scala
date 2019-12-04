35b78a93d0a2f1e6b1013f431206558e67c7ebb9
Naval Gupta <naval.gupta@workday.com>
2019-12-03 21:32:51 -0800

    dont count the same line crossing itself as 'X'

--- probs/src/day3/day3.scala
+++ probs/src/day3/day3.scala
@@ -1,29 +1,58 @@
 package day3
 
-import java.util
-
 import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
+import scala.io.Source
 
 object day3 {
 
+  val size = 30000
+  val wire1Traversal = new mutable.HashMap[(Int, Int), Int]
+  val wire2Traversal = new mutable.HashMap[(Int, Int), Int]
+
   //traverse single input like R45 or L3
-  def traverseDirectionAndGetEndingIndices(input: String, startingPoint: (Int, Int), arr: Array[Array[String]]) :(Int, Int) = {
+  def traverseDirectionAndGetEndingIndices(input: String, startingPoint: (Int, Int), arr: Array[Array[Char]], wireInfo: Int) :(Int, Int) = {
     val direction = input.substring(0,1)
     val traversableLength = input.substring(1).toInt
     var (endRowIndex, endColumnIndex) = startingPoint
-    var symbol = "-"
+    var symbol = wireInfo.toString.charAt(0)
     for(i <- 0 to traversableLength - 1) {
       direction match {
-        case "R" => endColumnIndex += 1; symbol = "-"
-        case "U" => endRowIndex -= 1; symbol = "|"
-        case "L" => endColumnIndex -= 1; symbol = "-"
-        case "D" => endRowIndex += 1; symbol = "|"
+        case "R" => endColumnIndex += 1//; symbol = '-'
+        case "U" => endRowIndex -= 1//; symbol = '|'
+        case "L" => endColumnIndex -= 1//; symbol = '-'
+        case "D" => endRowIndex += 1//; symbol = '|'
       }
-      if(arr(endRowIndex)(endColumnIndex) == ".")
+      wireInfo match {
+        case 1 => {
+          if(wire1Traversal.contains((endRowIndex, endColumnIndex))){
+            wire1Traversal.put((endRowIndex, endColumnIndex), wire1Traversal.get((endRowIndex, endColumnIndex)).get+1)
+          } else {
+            wire1Traversal += (endRowIndex, endColumnIndex) -> 1
+          }
+        }
+        case 2 => {
+          if(wire2Traversal.contains((endRowIndex, endColumnIndex))){
+            wire2Traversal.put((endRowIndex, endColumnIndex), wire2Traversal.get((endRowIndex, endColumnIndex)).get+1)
+          } else {
+            wire2Traversal += (endRowIndex, endColumnIndex) -> 1
+          }
+        }
+      }
+
+      if(arr(endRowIndex)(endColumnIndex) == '.')
         arr(endRowIndex)(endColumnIndex) = symbol
-      else
-        arr(endRowIndex)(endColumnIndex) = "X"
+      else {
+
+        if(wireInfo == 1 && wire1Traversal.get(endRowIndex, endColumnIndex).get > 1) {
+          arr(endRowIndex)(endColumnIndex) = '1'
+          println(s"wire1 crossing itself again at ${(endRowIndex, endColumnIndex)}")
+
+
+        } else if(wireInfo == 2 && wire2Traversal.get(endRowIndex, endColumnIndex).get > 1) {
+          arr(endRowIndex)(endColumnIndex) = '2'
+          println(s"wire2 crossing itself again at ${(endRowIndex, endColumnIndex)}")
+        } else arr(endRowIndex)(endColumnIndex) = 'X'
+      }
     }
     //arr(endRowIndex)(endColumnIndex) = "+"  //can cause confusion if the 2 wires intersect at + so leave it as is.
     //println((endRowIndex, endColumnIndex))
@@ -32,14 +61,17 @@ object day3 {
 
 
   //traverse single wire input like "R8","U5","L5","D3"
-  def traverseWire(wire: Array[String], arr: Array[Array[String]], startingPoint: (Int, Int)) = {
+  def traverseWire(wire: Array[String], arr: Array[Array[Char]], startingPoint: (Int, Int), wireInfo: Int) = {
     var endingIndices = startingPoint
+    var i =0
     for(path <- wire) {
-       endingIndices = traverseDirectionAndGetEndingIndices(path, endingIndices, arr)
+      endingIndices = traverseDirectionAndGetEndingIndices(path, endingIndices, arr, wireInfo)
+      i += 1
+      //println(s"iteration: $i, path: $path, indices: ${endingIndices}")
     }
   }
 
-  def printArr(arr: Array[Array[String]]) = {
+  def printArr(arr: Array[Array[Char]]) = {
     for(i <- 0 to arr.size-1) {
       for(j <- 0 to arr.size-1) {
         print(arr(i)(j))
@@ -48,19 +80,19 @@ object day3 {
     }
   }
 
-  def assignDot(arr: Array[Array[String]]) = {
+  def assignDot(arr: Array[Array[Char]]) = {
     for(i <- 0 to arr.size-1) {
       for(j <- 0 to arr.size-1) {
-        arr(i)(j) = "."
+        arr(i)(j) = '.'
       }
     }
   }
 
-  def getIntersections(arr: Array[Array[String]]) : mutable.Queue[(Int, Int)] = {
+  def getIntersections(arr: Array[Array[Char]]) : mutable.Queue[(Int, Int)] = {
     val intersections = mutable.Queue[(Int, Int)]()
     for(i <- 0 to arr.size-1) {
       for(j <- 0 to arr.size-1) {
-        if(arr(i)(j) == "X") {
+        if(arr(i)(j) == 'X') {
           intersections.enqueue((i,j))
         }
       }
@@ -69,23 +101,40 @@ object day3 {
   }
 
 
-  def getIntersectionPoints(wire1: Array[String], wire2: Array[String]): List[String]  = {
-    val size = 30
-    val arr = Array.ofDim[String](size, size)
+  def getIntersectionPoints(wire1: Array[String], wire2: Array[String]): Int  = {
+    val arr = Array.ofDim[Char](size, size)
     assignDot(arr)
-    arr(size/2)(size/2) = "O" //assign the central port
-
-    //traverse wire1
-    traverseWire(wire1, arr, (size/2, size/2))
-    traverseWire(wire2, arr, (size/2, size/2))
-    printArr(arr)
+    arr(size/2)(size/2) = 'O' //assign the central port
+    val centre = (size/2,size/2)
+    //traverse wires
+    traverseWire(wire1, arr, centre, 1)
+    traverseWire(wire2, arr, centre, 2)
+    //printArr(arr)
+    val distanceList = new scala.collection.mutable.ListBuffer[Int]
+    println(s"\n\ncentral point -> ${centre}")
     for(intersection <- getIntersections(arr)) {
-      println(intersection)
+      val ds = (centre._1 - intersection._1).abs + (centre._2 - intersection._2).abs
+      distanceList += ds
+      println(s"Intersection -> $intersection Distance -> $ds")
     }
-    null
+    println(s"minimum DS -> ${distanceList.min}")
+    distanceList.min
   }
 
   def main(args: Array[String]): Unit = {
     getIntersectionPoints(Array("R8","U5","L5","D3"), Array("U7","R6","D4","L4"))
+
+    println("===================")
+    getIntersectionPoints(Array("R75","D30","R83","U83","L12","D49","R71","U7","L72"), Array("U62","R66","U55","R34","D71","R55","D58","R83"))
+
+    println("===================")
+    getIntersectionPoints(Array("R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"),
+      Array("U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"))
+
+    println("===================")
+    val file = "/Users/naval.gupta/code/advent-of-code-2019/probs/src/day3/input-day3.txt"
+    val wire1 = Source.fromFile(file).getLines().toList(0).split(",")
+    val wire2 = Source.fromFile(file).getLines().toList(1).split(",")
+    getIntersectionPoints(wire1, wire2)
   }
 }
