fed8e042df8bbb214940ce199ec9a5e5d6f70292
Naval Gupta <naval.gupta@workday.com>
2019-12-09 15:34:38 -0800

    solved day8 part1

--- probs/src/day8/day8.scala
+++ probs/src/day8/day8.scala
@@ -1,35 +1,70 @@
 package day8
 
+import scala.collection.mutable.HashMap
 import scala.io.Source
 
+case class tracker(Zeros: Int, Ones: Int, Twos: Int)
 object day8 {
 
   def layout(width: Int, tall: Int, digits: Seq[Int]) : Array[Array[Array[Int]]] = {
-    val numlayers = digits.size / (tall * width)
-    val layer = Array.ofDim[Int](numlayers, tall, width)
+    val numLayers = digits.size / (tall * width)
+    var digitTracker = HashMap.empty[Int, tracker]
+    val layer = Array.ofDim[Int](numLayers, tall, width)
     var counter = 0
+
     def getNextDigit() = {
       digits(counter)
     }
 
-    for (z <- 0 to numlayers-1) {
+    def incTracker(whichDigit: Int, layerNum: Int) = {
+      if(!digitTracker.contains(layerNum)) {
+        digitTracker += (layerNum -> new tracker(0,0,0))
+      }
+      val trc = digitTracker.get(layerNum).get
+      whichDigit match {
+        case 0 => digitTracker.put(layerNum, new tracker(trc.Zeros+1, trc.Ones, trc.Twos))
+        case 1 => digitTracker.put(layerNum, new tracker(trc.Zeros, trc.Ones+1, trc.Twos))
+        case 2 => digitTracker.put(layerNum, new tracker(trc.Zeros, trc.Ones, trc.Twos+1))
+        case _ => //meh
+      }
+    }
+
+    def printLayerDetails(whichLayer: Int) = {
+      println(s"Layer: ${whichLayer+1}, 0's: ${digitTracker.get(whichLayer).get.Zeros}, 1's: ${digitTracker.get(whichLayer).get.Ones}, 2's: ${digitTracker.get(whichLayer).get.Twos}")
+    }
+
+    var layerWithMinZeros = 0
+    var numOfMinZeros = Int.MaxValue
+    for (z <- 0 until numLayers) {
       print(s"layer:${z + 1}\n")
-      for (i <- 0 to tall - 1) {
-        for (j <- 0 to width - 1) {
+      for (i <- 0 until tall) {
+        for (j <- 0 until width) {
           layer(z)(i)(j) = getNextDigit()
+
+          getNextDigit() match {
+            case a@(0 | 1 | 2) => incTracker(a, z)
+            case _ => //meh
+          }
           counter += 1
           print(s"${layer(z)(i)(j)} ")
         }
         println("")
       }
+      printLayerDetails(z)
+
+      if(digitTracker.get(z).get.Zeros < numOfMinZeros) {
+        //println(s"got layer with min zeros: ${digitTracker.get(z).get.Zeros}, prev layer: ")
+        layerWithMinZeros = z
+        numOfMinZeros = digitTracker.get(z).get.Zeros
+      }
       println()
     }
-
+    println(s"Minimum zeros in layer ${layerWithMinZeros+1}, 1's * 2's = ${digitTracker.get(layerWithMinZeros).get.Ones *  digitTracker.get(layerWithMinZeros).get.Twos}")
     layer
   }
 
   def main(args: Array[String]): Unit = {
-    val x = "123456789012".split("").toSeq.map( _.toInt)
+    val x = "123250122112123456".split("").toSeq.map( _.toInt)
     layout(3,2, x)
     println("=======================")
 
