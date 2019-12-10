122e7a9cc2b9b83907bc009a207fafb53d6c6892
Naval Gupta <naval.gupta@workday.com>
2019-12-09 22:23:28 -0800

    day 8, part 2 G1

--- probs/src/day8/day8.scala
+++ probs/src/day8/day8.scala
@@ -1,14 +1,15 @@
 package day8
 
-import scala.collection.mutable.HashMap
+import scala.collection.mutable._
 import scala.io.Source
+import util.control.Breaks._
 
 case class tracker(Zeros: Int, Ones: Int, Twos: Int)
 object day8 {
 
-  def layout(width: Int, tall: Int, digits: Seq[Int]) : Array[Array[Array[Int]]] = {
+  def layout(width: Int, tall: Int, digits: collection.Seq[Int]) : Array[Array[Array[Int]]] = {
     val numLayers = digits.size / (tall * width)
-    var digitTracker = HashMap.empty[Int, tracker]
+    var digitTracker = collection.mutable.HashMap.empty[Int, tracker]
     val layer = Array.ofDim[Int](numLayers, tall, width)
     var counter = 0
 
@@ -42,7 +43,7 @@ object day8 {
           layer(z)(i)(j) = getNextDigit()
 
           getNextDigit() match {
-            case a@(0 | 1 | 2) => incTracker(a, z)
+            case a@(0 | 1 | 2) => incTracker(a, z) //umm now i realize that i could maintain a counter instead of updating always.
             case _ => //meh
           }
           counter += 1
@@ -53,7 +54,6 @@ object day8 {
       printLayerDetails(z)
 
       if(digitTracker.get(z).get.Zeros < numOfMinZeros) {
-        //println(s"got layer with min zeros: ${digitTracker.get(z).get.Zeros}, prev layer: ")
         layerWithMinZeros = z
         numOfMinZeros = digitTracker.get(z).get.Zeros
       }
@@ -63,6 +63,30 @@ object day8 {
     layer
   }
 
+  def decodeImage(img: Array[Array[Array[Int]]], width: Int, tall: Int): Unit = {
+    println("___DecodeImage___")
+    for(i <- 0 until tall) {
+      for (j <- 0 until width) {
+        //get all the layers now
+        breakable {
+          for (z <- 0 until img.size) {
+
+            val digit = img(z)(i)(j)
+            val x = ""
+
+            digit match {
+              case 0 => print(" ");break()
+              case 1 => print("*");break()
+              case 2 => //no_op
+            }
+
+          }
+        }
+      }
+      println()
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     val x = "123250122112123456".split("").toSeq.map( _.toInt)
     layout(3,2, x)
@@ -70,6 +94,7 @@ object day8 {
 
     val file = "/Users/naval.gupta/code/advent-of-code-2019/probs/src/day8/input.txt"
     val lst  = Source.fromFile(file).getLines().toList(0).split("").toSeq.map( _.toInt)
-    layout(25, 6, lst)
+    val xx = layout(25, 6, lst)
+    decodeImage(xx, 25, 6)
   }
 }
